bf70056b7410bb6c47bdd8034b8cacaf7dc7194b
MaximMaximS <sklenicka.maxim@gmail.com>
2024-06-13 23:01:49 +0200

    Works

--- src/bakalari.rs
+++ src/bakalari.rs
@@ -1,15 +1,168 @@
-use rezvrh_scraper::Bakalari;
+use chrono::{DateTime, Duration};
+use chrono_tz::{Europe::Prague, Tz};
+use rezvrh_scraper::{Bakalari, Selector, Timetable, Type};
+use tokio::sync::Mutex;
 
+#[derive(Debug)]
+pub struct Options {
+    /// Seconds
+    pub lights_on: u32,
+    /// Seconds
+    pub lights_off: u32,
+    /// Seconds
+    pub before_break: u32,
+    /// Seconds
+    pub before_lesson: u32,
+}
+
+#[derive(Debug)]
 pub struct BakaWrapper {
     bakalari: Bakalari,
+    selector: Selector,
+    options: Options,
+    timetable: Mutex<Option<Timetable>>,
+}
+
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
+pub enum State {
+    Lesson,
+    BeforeLesson,
+    Break,
+    BeforeBreak,
+    Empty,
 }
 
 impl BakaWrapper {
-    pub const fn new(bakalari: Bakalari) -> Self {
-        Self { bakalari }
+    pub fn new(bakalari: Bakalari, room: &str, options: Options) -> Option<Self> {
+        let sel = bakalari.get_selector(Type::Room, room);
+
+        Some(Self {
+            bakalari,
+            selector: sel?,
+            timetable: Mutex::new(None),
+            options,
+        })
     }
 
-    pub const fn get_light(&self) -> u8 {
-        todo!()
+    pub async fn get_state(&self) -> anyhow::Result<State> {
+        let now = chrono::Local::now().with_timezone(&Prague);
+        self.get_state_at(now).await
+    }
+
+    pub async fn get_state_at(&self, date: DateTime<Tz>) -> anyhow::Result<State> {
+        let mut table_l = self.timetable.lock().await;
+        if table_l.is_none() {
+            *table_l = Some(
+                self.bakalari
+                    .get_timetable(rezvrh_scraper::Which::Actual, &self.selector)
+                    .await?,
+            );
+        }
+
+        let table = table_l.as_ref().unwrap().clone();
+        drop(table_l);
+
+        let time_now = date.time();
+        let date_now = date.date_naive();
+        let day = table.days.iter().find(|d| d.date == Some(date_now));
+        let Some(day) = day else {
+            println!("No day found for {date_now:?}");
+            return Ok(State::Empty);
+        };
+        let hours = &table.hours;
+        assert!(hours.len() == day.lessons.len());
+        let mut lessons = hours
+            .iter()
+            .zip(day.lessons.iter())
+            .filter(|(_, l)| {
+                if l.is_empty() {
+                    return false;
+                }
+                let l = &l[0];
+                matches!(
+                    l,
+                    rezvrh_scraper::Lesson::Regular { .. }
+                        | rezvrh_scraper::Lesson::Substitution { .. }
+                )
+            })
+            .peekable();
+        let first = lessons.peek();
+        let Some(first) = first else {
+            return Ok(State::Empty);
+        };
+        let lights_on = first.0.start - Duration::seconds(i64::from(self.options.lights_on));
+        if time_now < lights_on {
+            return Ok(State::Empty);
+        }
+        while let Some((hour, _)) = lessons.next() {
+            let start = hour.start;
+            let early_start = start - Duration::seconds(i64::from(self.options.before_lesson));
+
+            if time_now < early_start {
+                return Ok(State::Break);
+            }
+
+            if time_now < start {
+                return Ok(State::BeforeLesson);
+            }
+
+            let end = hour.start + Duration::minutes(hour.duration.into());
+            let early_end = end - Duration::seconds(i64::from(self.options.before_break));
+            if time_now < early_end {
+                return Ok(State::Lesson);
+            }
+
+            if time_now < end {
+                return Ok(State::BeforeBreak);
+            }
+
+            if lessons.peek().is_some() {
+                continue;
+            }
+
+            let lights_off = end + Duration::seconds(i64::from(self.options.lights_off));
+
+            if time_now < lights_off {
+                return Ok(State::Break);
+            }
+
+            return Ok(State::Empty);
+        }
+        unreachable!("lessons.next() should have returned None");
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use chrono::Timelike;
+
+    use super::*;
+
+    #[tokio::test]
+    async fn test_get_state() {
+        let baka = Bakalari::no_auth("https://gymberoun.bakalari.cz".parse().unwrap())
+            .await
+            .unwrap();
+        let wrapper = BakaWrapper::new(
+            baka,
+            "216",
+            Options {
+                lights_on: 30 * 60,
+                lights_off: 10 * 60,
+                before_break: 60,
+                before_lesson: 60,
+            },
+        )
+        .unwrap();
+
+        let mut date = chrono::Local::now()
+            .with_timezone(&Prague)
+            .with_hour(6)
+            .unwrap();
+        for _ in 0..1000 {
+            let state = wrapper.get_state_at(date).await.unwrap();
+            println!("{date:?} {state:?}");
+            date += Duration::minutes(1);
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use anyhow::Context;
 use axum::{
+    http::StatusCode,
+    response::{IntoResponse, Response},
     routing::get,
     Router,
 };
@@ -7,6 +9,27 @@ use rezvrh_scraper::Bakalari;
 use std::env::var;
 
 mod bakalari;
+
+struct AppError(anyhow::Error);
+
+impl IntoResponse for AppError {
+    fn into_response(self) -> Response {
+        (
+            StatusCode::INTERNAL_SERVER_ERROR,
+            format!("Something went wrong: {}", self.0),
+        )
+            .into_response()
+    }
+}
+
+impl<E> From<E> for AppError
+where
+    E: Into<anyhow::Error>,
+{
+    fn from(err: E) -> Self {
+        Self(err.into())
+    }
+}
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
@@ -16,8 +39,27 @@ async fn main() -> anyhow::Result<()> {
     let port = var("PORT").unwrap_or_else(|_| "3000".to_string());
 
     // Baka
+    let room = var("BAKA_ROOM").context("BAKA_ROOM not set")?;
     let baka_url = var("BAKA_URL").context("BAKA_URL not set")?.parse()?;
     let baka_auth = var("BAKA_AUTH").ok();
+
+    let lights_on = var("LIGHTS_ON")
+        .context("LIGHTS_ON not set")?
+        .parse::<u32>()
+        .context("Invalid LIGHTS_ON")?;
+    let lights_off = var("LIGHTS_OFF")
+        .context("LIGHTS_OFF not set")?
+        .parse::<u32>()
+        .context("Invalid LIGHTS_OFF")?;
+    let before_break = var("BEFORE_BREAK")
+        .context("BEFORE_BREAK not set")?
+        .parse::<u32>()
+        .context("Invalid BEFORE_BREAK")?;
+    let before_lesson = var("BEFORE_LESSON")
+        .context("BEFORE_LESSON not set")?
+        .parse::<u32>()
+        .context("Invalid BEFORE_LESSON")?;
+
     let creds = if let Some(auth) = baka_auth {
         let (username, password) = auth.split_once(':').context("Invalid BAKA_AUTH")?;
         Some((username.to_owned(), password.to_owned()))
@@ -31,7 +73,15 @@ async fn main() -> anyhow::Result<()> {
         Bakalari::no_auth(baka_url).await?
     };
 
-    let bakalari = bakalari::BakaWrapper::new(bakalari);
+    let options = bakalari::Options {
+        lights_on,
+        lights_off,
+        before_break,
+        before_lesson,
+    };
+    let bakalari = bakalari::BakaWrapper::new(bakalari, &room, options).context("Invalid room")?;
+    let state = bakalari.get_state().await?;
+    println!("{state:?}");
 
     let app = Router::new().route("/", get(|| async { "Hello, World!" }));
 
