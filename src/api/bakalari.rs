ef38baf7c6cedf91a811eee4688421ab7695faeb
MaximMaximS <sklenicka.maxim@gmail.com>
2024-06-19 10:07:28 +0200

    Move

--- src/api/bakalari.rs
+++ src/api/bakalari.rs
@@ -1,3 +1,6 @@
+use std::sync::Arc;
+
+use axum::extract::State;
 use chrono::{DateTime, Duration};
 use chrono_tz::{Europe::Prague, Tz};
 use rezvrh_scraper::{Bakalari, Selector, Timetable, Type};
@@ -5,6 +8,8 @@ use tokio::sync::Mutex;
 use tracing::debug;
 
 use crate::CONFIG;
+
+use super::util::AppError;
 
 #[derive(Debug)]
 pub struct BakaWrapper {
@@ -14,7 +19,7 @@ pub struct BakaWrapper {
 }
 
 #[derive(Debug, PartialEq, Eq, Copy, Clone)]
-pub enum State {
+pub enum LightState {
     Empty,
     Break,
     BeforeLesson,
@@ -22,7 +27,7 @@ pub enum State {
     BeforeBreak,
 }
 
-impl State {
+impl LightState {
     pub const fn light(self) -> u8 {
         match self {
             Self::Empty => 0b000,
@@ -46,13 +51,13 @@ impl BakaWrapper {
     }
 
     #[tracing::instrument(skip(self))]
-    pub async fn get_state(&self) -> anyhow::Result<State> {
+    pub async fn get_state(&self) -> anyhow::Result<LightState> {
         let now = chrono::Local::now().with_timezone(&Prague);
         self.get_state_at(now).await
     }
 
     #[tracing::instrument(skip(self))]
-    pub async fn get_state_at(&self, date: DateTime<Tz>) -> anyhow::Result<State> {
+    pub async fn get_state_at(&self, date: DateTime<Tz>) -> anyhow::Result<LightState> {
         let mut table_l = self.timetable.lock().await;
         if table_l.is_none() {
             *table_l = Some(
@@ -71,7 +76,7 @@ impl BakaWrapper {
         let day = table.days.iter().find(|d| d.date == Some(date_now));
         let Some(day) = day else {
             println!("No day found for {date_now:?}");
-            return Ok(State::Empty);
+            return Ok(LightState::Empty);
         };
         let hours = &table.hours;
         assert!(hours.len() == day.lessons.len());
@@ -94,35 +99,35 @@ impl BakaWrapper {
         debug!(first = ?first, "First lesson");
         let Some(first) = first else {
             debug!("No lessons");
-            return Ok(State::Empty);
+            return Ok(LightState::Empty);
         };
         let tz_start = date.with_time(first.0.start).unwrap();
         debug!(tz_start = ?tz_start, "First lesson start");
         let lights_on = tz_start - CONFIG.time.lights_on;
         if date < lights_on {
             debug!("Too early for lights on");
-            return Ok(State::Empty);
+            return Ok(LightState::Empty);
         }
         while let Some((hour, _)) = lessons.next() {
             let start = hour.start;
             let early_start = start - CONFIG.time.before_lesson;
 
             if time_now < early_start {
-                return Ok(State::Break);
+                return Ok(LightState::Break);
             }
 
             if time_now < start {
-                return Ok(State::BeforeLesson);
+                return Ok(LightState::BeforeLesson);
             }
 
             let end = hour.start + Duration::minutes(hour.duration.into());
             let early_end = end - CONFIG.time.before_break;
             if time_now < early_end {
-                return Ok(State::Lesson);
+                return Ok(LightState::Lesson);
             }
 
             if time_now < end {
-                return Ok(State::BeforeBreak);
+                return Ok(LightState::BeforeBreak);
             }
 
             if lessons.peek().is_some() {
@@ -132,11 +137,16 @@ impl BakaWrapper {
             let lights_off = end + CONFIG.time.lights_off;
 
             if time_now < lights_off {
-                return Ok(State::Break);
+                return Ok(LightState::Break);
             }
 
-            return Ok(State::Empty);
+            return Ok(LightState::Empty);
         }
         unreachable!("lessons.next() should have returned None");
     }
+}
+
+pub async fn get_light(State(baka): State<Arc<BakaWrapper>>) -> Result<String, AppError> {
+    let state = baka.get_state().await?;
+    Ok(format!("{}", state.light() | 0b1000))
 }
