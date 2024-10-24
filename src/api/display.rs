126c24c5a996d4d49c4b0aac1a21062c584d027c
MaximMaximS <sklenicka.maxim@gmail.com>
2024-10-24 08:37:30 +0200

    Add random mode

--- src/api/display.rs
+++ src/api/display.rs
@@ -4,12 +4,13 @@ use std::sync::Arc;
 
 pub async fn get_live(State(state): State<Arc<AppState>>) -> Result<String, AppError> {
     let config = state.state.lock().await;
-    let static_val = config.custom.to_val();
     let mode = config.mode;
+    let custom = config.custom;
     drop(config);
     let light = match mode {
-        Mode::Static => static_val,
-        Mode::Bakalari => state.bakalari.get_state().await?.light().to_val(),
+        Mode::Static => custom,
+        Mode::Bakalari => state.bakalari.get_state().await?.light(),
+        Mode::Random => rand::random(),
     };
-    Ok(format!("{}", light | 0b1000))
+    Ok(format!("{}", light.to_val() | 0b1000))
 }
--- src/api/state.rs
+++ src/api/state.rs
@@ -1,3 +1,4 @@
+use rand::{distributions::Standard, prelude::Distribution, Rng};
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy, Hash, Default, Serialize, Deserialize)]
@@ -5,6 +6,7 @@ pub enum Mode {
     #[default]
     Bakalari,
     Static,
+    Random,
 }
 
 #[derive(Debug, PartialEq, Eq, Clone, Copy, Hash, Default, Serialize, Deserialize)]
@@ -20,6 +22,13 @@ pub enum Light {
     RedAmberGreen,
 }
 
+impl Distribution<Light> for Standard {
+    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Light {
+        let val = rng.gen_range(0..=7);
+        Light::from_val(val)
+    }
+}
+
 impl Light {
     pub const fn to_val(self) -> u8 {
         match self {
@@ -31,6 +40,19 @@ impl Light {
             Self::RedGreen => 0b101,
             Self::AmberGreen => 0b110,
             Self::RedAmberGreen => 0b111,
+        }
+    }
+
+    pub const fn from_val(val: u8) -> Self {
+        match val {
+            0b001 => Self::Red,
+            0b010 => Self::Amber,
+            0b011 => Self::RedAmber,
+            0b100 => Self::Green,
+            0b101 => Self::RedGreen,
+            0b110 => Self::AmberGreen,
+            0b111 => Self::RedAmberGreen,
+            _ => Self::Off,
         }
     }
 }
